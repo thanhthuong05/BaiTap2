bd2cbee9cbb3ba729dc15085a9c955809a256fce
ADMIN <ADMIN@PC330>
2024-01-03 07:58:33 +0700

    Add project files.

---
+++ BaiTap2/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTap2
+{
+    class Acccount
+    {
+        private string id;
+        private string name;
+        private int balance;
+
+        public Acccount(string id, string name, int balance)
+        {
+            this.id = id;
+            this.name = name;
+            this.balance = balance;
+        }
+        public string getID()
+        {
+            return id;
+        }
+        public string getName()
+        {
+            return name;
+        }
+        public int getBalance()
+        {
+            return balance;
+        }
+        public void credit(int amount)
+        {
+            if (amount > 0)
+                balance += amount;
+        }
+        public void debit(int amount)
+        {
+            if (amount <= balance)
+                balance -= amount;
+            else
+                Console.WriteLine("So tien vuot qua so du. Rut tien khong thanh cong");
+
+        }
+        public void tranFerTo(Acccount acc, int amount)
+        {
+            if (amount <= balance)
+            {
+                balance -= amount;
+                acc.balance += amount;
+            }
+            else
+            {
+                Console.WriteLine("So tien vuot so du. Chuyen khoan khong thanh cong:");
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Acccount my = new Acccount("900111", "Le Van Ti", 600);
+            Acccount myFriend = new Acccount("900112", "Tran Van Ti", 30);
+            Console.WriteLine("---------Thong tin 2 tai khoan ban dau-------");
+            Console.WriteLine("My Account: [ id;{0}, name : {1}, blance: {2}",
+            my.getID(), my.getName(), my.getBalance());
+            Console.WriteLine("My Friend Account: [ id;{0}, name : {1}, blance: {2}",
+           myFriend.getID(), myFriend.getName(), myFriend.getBalance());
+
+            // RUT TIEN 
+
+            my.debit(200);
+            my.tranFerTo(myFriend, 450);
+
+            //ket qua sau khi thuc hien mot so hanh vi(nap, rut, chuyen khoan)
+
+            Console.WriteLine("---------Thong tin 2 tai khoan ban dau-------");
+            Console.WriteLine("My Account: [ id;{0}, name : {1}, blance: {2}",
+            my.getID(), my.getName(), my.getBalance());
+            Console.WriteLine("My Friend Account: [ id;{0}, name : {1}, blance: {2}",
+            myFriend.getID(), myFriend.getName(), myFriend.getBalance());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BaiTap3/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTap3
+{
+    class phanso
+    {
+        private int tuso, mauso;
+
+        public phanso()
+        {
+            tuso = 0;
+            mauso = 1;
+        }
+        public phanso(int tuso, int mauso)
+        {
+            this.tuso = tuso;
+            this.mauso = mauso;
+        }
+        public void settuso(int tuso)
+        {
+            this.tuso = tuso;
+        }
+        public int gettuso()
+        {
+            return tuso;
+        }
+        public void setMauso(int mauso)
+        {
+            this.mauso = mauso;
+        }
+        public int getMauso()
+        {
+            return mauso;
+        }
+        public void toiGian()
+        {
+            int uscln = TimUSCLN(tuso, mauso);
+            tuso = tuso / uscln;
+            mauso = mauso / uscln;
+        }
+        private int TimUSCLN(int a, int b)
+        {
+            int x = Math.Abs(a);
+            int y = Math.Abs(b);
+            while (x != y)
+            {
+                if (x > y)
+                    x = x - y;
+                else
+                    y = y - x;
+            }
+            return x;
+        }
+        public phanso cong(phanso ps)
+        {
+            phanso kq = new phanso();
+            kq.tuso = tuso * ps.mauso + ps.tuso * mauso;
+            kq.mauso = mauso * ps.mauso;
+            return kq;
+        }
+        public phanso tru(phanso ps)
+        {
+            phanso kq = new phanso();
+            kq.tuso = tuso * ps.mauso - ps.tuso * mauso;
+            kq.mauso = mauso * ps.mauso;
+            return kq;
+        }
+        public phanso nhan(phanso ps)
+        {
+            phanso kq = new phanso();
+            kq.tuso = tuso * ps.mauso;
+            kq.mauso = mauso * ps.mauso;
+            return kq;
+        }
+        public phanso chia(phanso ps)
+        {
+            phanso kq = new phanso();
+            kq.tuso = tuso * ps.mauso + ps.tuso * mauso;
+            kq.mauso = mauso * ps.mauso;
+            return kq;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //câu a
+            phanso p1 = new phanso();
+            Console.WriteLine("******Nhap thong tin phan so thu 1: ");
+            Console.Write("Nhap tu so");
+            int tuso = int.Parse(Console.ReadLine());
+            Console.Write("Nhap mau so; ");
+            int mauso = int.Parse(Console.ReadLine());
+            phanso p2 = new phanso(tuso, mauso);
+            p2.toiGian();
+            phanso tong = p1.cong(p2);
+            Console.WriteLine("Tong hai phan so PS1{0/{1}, PS2[{2}/{3}] = KQ[{4}/{5}]",
+            p1.gettuso(), p1.getMauso(), p2.gettuso(), p2.getMauso());
+            Console.ReadLine();
+    }
+    }
+}
